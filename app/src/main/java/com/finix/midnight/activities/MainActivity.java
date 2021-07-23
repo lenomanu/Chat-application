cb83dda38360f792d6710d9e4e7d41cbe9d9f306
lenomanu <emkipkosgei@gmail.com>
2021-07-23 15:50:35 +0300

    Chat Application

--- app/src/main/java/com/finix/midnight/activities/MainActivity.java
+++ app/src/main/java/com/finix/midnight/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.finix.midnight;
+package com.finix.midnight.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.finix.midnight.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/finix/midnight/activities/SignUpActivity.java
+++ app/src/main/java/com/finix/midnight/activities/SignUpActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Toast;
 
 import com.finix.midnight.R;
 import com.finix.midnight.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
     private ActivitySignUpBinding binding;
+    public String encodedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +24,57 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     public void setListeners(){
-        binding.textSignUp.setOnClickListener(v ->
-                startActivity(new Intent(getApplicationContext(),SignInActivity.class)));
-
+        binding.textSignUp.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),SignInActivity.class)));
+        binding.buttonSignUp.setOnClickListener(v -> {
+            if(isValidSignUpDetails()){
+                signUp();
+            }
+        });
     }
 
+    public void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+    public void signUp(){
+
+    }
+    public Boolean isValidSignUpDetails (){
+        if(encodedImage == null){
+            showToast("Select Profile picture");
+            return false;
+        }else if(binding.inputEmail.getText().toString().trim().isEmpty()){
+            showToast("Enter email");
+            return false;
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString().trim()).matches()){
+            showToast("Enter a valid email");
+            return false;
+        }else if(binding.inputName.getText().toString().trim().isEmpty()){
+            showToast("Enter Name");
+            return false;
+        }else if(binding.inputPassword.getText().toString().trim().isEmpty()){
+            showToast("Enter password");
+            return false;
+        }else if(binding.inputPassword.getText().toString().trim().length()<6){
+            showToast("Password should be more then six characters");
+            return false;
+        }else if(binding.inputConfirmPassword.getText().toString().trim().isEmpty()){
+            showToast("Please Confirm password");
+            return false;
+        }else if(binding.inputPassword.getText().toString().trim()!=binding.inputConfirmPassword.getText().toString().trim()){
+            showToast("Password and Confirm Password Must be same");
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+    public void loading(Boolean isLoading){
+        if(isLoading){
+            binding.buttonSignUp.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }else{
+            binding.buttonSignUp.setVisibility(View.VISIBLE);
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
 }
