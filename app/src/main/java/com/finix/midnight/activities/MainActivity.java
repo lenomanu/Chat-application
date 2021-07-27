0cb4422222009044bb39fca28ccd6250f1547582
lenomanu <emkipkosgei@gmail.com>
2021-07-27 17:17:39 +0300

    Chat Application

--- app/src/main/java/com/finix/midnight/activities/MainActivity.java
+++ app/src/main/java/com/finix/midnight/activities/MainActivity.java
@@ -2,15 +2,84 @@ package com.finix.midnight.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.Toast;
 
 import com.finix.midnight.R;
+import com.finix.midnight.databinding.ActivityMainBinding;
+import com.finix.midnight.utilities.Constants;
+import com.finix.midnight.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.text.Format;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        loadUserDetails();
+        setListeners();
+        getToken();
+    }
+
+    private void loadUserDetails() {
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE),Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+        binding.imageProfile.setImageBitmap(bitmap);
+    }
+
+    private void setListeners() {
+        binding.imageSignOut.setOnClickListener(v -> {
+            signOut();
+        });
+        binding.fabNewChat.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(),UsersActivity.class));
+        });
+
+    }
+
+    private void signOut() {
+        showToastMessage("Signing out...");
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID));
+        HashMap<String,Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates).addOnSuccessListener(unused -> {
+            preferenceManager.clear();
+            Intent intent = new Intent(getApplicationContext(),SignInActivity.class);
+            startActivity(intent);
+            finish();
+        }).addOnFailureListener(e -> showToastMessage("Unable to sign out now"));
+
+    }
+    private void showToastMessage(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+    private void uploadToken(String token){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID));
+        documentReference.update(Constants.KEY_FCM_TOKEN,token)
+                .addOnCompleteListener(unused->showToastMessage("Upload token successful"))
+                .addOnFailureListener(e ->showToastMessage("Unable to upload token"));
+    }
+    private void getToken(){
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::uploadToken);
     }
 }
---
+++ app/src/main/java/com/finix/midnight/activities/UsersActivity.java
@@ -0,0 +1,16 @@
+package com.finix.midnight.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.finix.midnight.R;
+
+public class UsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+    }
+}
